16d5786b7b0e1cfbe487cb09e1c8baa97f76a7ae
unkatoco <katoco@mit.edu>
2016-02-12 12:30:09 -0500

    tried adding some files

---
+++ Connection32/Connection32/Connection32.cpp
@@ -0,0 +1,13 @@
+// Connection32.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "ThorLabs.MotionControl.ModularRack.h"
+
+
+int main()
+{
+	TLI_BuildDeviceList();
+    return 0;
+}
+
